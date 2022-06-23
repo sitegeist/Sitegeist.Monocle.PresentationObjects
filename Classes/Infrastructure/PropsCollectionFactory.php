cb85b08577abbfeb329b96ead4e864f5138148b9
Bernhard Schmitt <schmitt@sitegeist.de>
2022-06-23 18:15:44 +0200

    Implement PropsCollectionFactory based on PO prop types

--- Classes/Infrastructure/PropsCollectionFactory.php
+++ Classes/Infrastructure/PropsCollectionFactory.php
@@ -16,9 +16,25 @@ declare(strict_types=1);
 namespace Sitegeist\Monocle\PresentationObjects\Infrastructure;
 
 use Neos\Flow\Annotations as Flow;
+use PackageFactory\AtomicFusion\PresentationObjects\Domain\Component\ComponentName;
+use PackageFactory\AtomicFusion\PresentationObjects\Domain\Component\Props;
+use PackageFactory\AtomicFusion\PresentationObjects\Domain\Component\PropType\BoolPropType;
+use PackageFactory\AtomicFusion\PresentationObjects\Domain\Component\PropType\EnumPropType;
+use PackageFactory\AtomicFusion\PresentationObjects\Domain\Component\PropType\FloatPropType;
+use PackageFactory\AtomicFusion\PresentationObjects\Domain\Component\PropType\IntPropType;
+use PackageFactory\AtomicFusion\PresentationObjects\Domain\Component\PropType\SlotPropType;
+use PackageFactory\AtomicFusion\PresentationObjects\Domain\Component\PropType\StringPropType;
+use PackageFactory\AtomicFusion\PresentationObjects\Domain\Component\PropType\UriPropType;
+use PackageFactory\AtomicFusion\PresentationObjects\Domain\Enum\PseudoEnumInterface;
+use PackageFactory\AtomicFusion\PresentationObjects\Domain\PackageKey;
 use Sitegeist\Monocle\Domain\Fusion\Prototype;
+use Sitegeist\Monocle\Domain\PrototypeDetails\Props\EditorFactory;
+use Sitegeist\Monocle\Domain\PrototypeDetails\Props\Prop;
+use Sitegeist\Monocle\Domain\PrototypeDetails\Props\PropName;
+use Sitegeist\Monocle\Domain\PrototypeDetails\Props\PropsCollection;
 use Sitegeist\Monocle\Domain\PrototypeDetails\Props\PropsCollectionFactoryInterface;
 use Sitegeist\Monocle\Domain\PrototypeDetails\Props\PropsCollectionInterface;
+use Sitegeist\Monocle\Domain\PrototypeDetails\Props\PropValue;
 
 /**
  * @Flow\Scope("singleton")
@@ -31,10 +47,81 @@ final class PropsCollectionFactory implements PropsCollectionFactoryInterface
      */
     protected $fusionContextName;
 
+    /**
+     * @Flow\Inject
+     * @var EditorFactory
+     */
+    protected $editorFactory;
+
     public function fromPrototypeForPrototypeDetails(
         Prototype $prototype
     ): PropsCollectionInterface {
-        \Neos\Flow\var_dump($prototype);
-        exit();
+        $componentName = ComponentName::fromInput(
+            $prototype->getName()->jsonSerialize(),
+            new PackageKey('Sitegeist.Monocle')
+        );
+
+        $monocleProps = [];
+        foreach (Props::fromClassName($componentName->getFullyQualifiedClassName()) as $propName => $propType) {
+            switch (get_class($propType)) {
+                case BoolPropType::class:
+                    $monocleProps[] = new Prop(
+                        PropName::fromString($propName),
+                        PropValue::fromAny(false),
+                        $this->editorFactory->checkbox()
+                    );
+                    break;
+                case EnumPropType::class:
+                    /** @var PseudoEnumInterface[] $cases */
+                    $cases = ($propType->getClassName())::cases();
+                    $defaultCase = reset($cases);
+                    $selectBoxOptions = [];
+                    foreach ($cases as $case) {
+                        $selectBoxOptions[] = [
+                            'value' => $case->getValue(),
+                            'label' => $case->getValue()
+                        ];
+                    }
+                    $monocleProps[] = new Prop(
+                        PropName::fromString($propName),
+                        PropValue::fromAny($defaultCase->getValue()),
+                        $this->editorFactory->selectBox([
+                            'options' => $selectBoxOptions
+                        ])
+                    );
+                    break;
+                case FloatPropType::class:
+                    $monocleProps[] = new Prop(
+                        PropName::fromString($propName),
+                        PropValue::fromAny(84.72),
+                        $this->editorFactory->number('float')
+                    );
+                    break;
+                case IntPropType::class:
+                    $monocleProps[] = new Prop(
+                        PropName::fromString($propName),
+                        PropValue::fromAny(8472),
+                        $this->editorFactory->number('integer')
+                    );
+                    break;
+                case StringPropType::class:
+                    $monocleProps[] = new Prop(
+                        PropName::fromString($propName),
+                        PropValue::fromAny('Text'),
+                        $this->editorFactory->text()
+                    );
+                    break;
+                case UriPropType::class:
+                    $monocleProps[] = new Prop(
+                        PropName::fromString($propName),
+                        PropValue::fromAny('https://neos.io'),
+                        $this->editorFactory->text()
+                    );
+                    break;
+                default:
+            }
+        }
+
+        return new PropsCollection(...$monocleProps);
     }
 }
